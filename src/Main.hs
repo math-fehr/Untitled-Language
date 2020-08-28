93448a602ed5e1394cd90369f9fb3aea7e1b117c
Thibaut Pérami <thibaut.perami@ens.fr>
2020-08-28 18:31:45 +0200

    Make the program build again

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,22 @@
 import Parser
 import ParsingToIR
+import System.Exit
 import Typing
 
-checkAndType parsed_ir = do
-  ir <- parsedProgramToIr parsed_ir
-  checkProgramWellTyped ir
-  return ir
-
+-- checkAndType parsed_ir = do
+--   ir <- parsedProgramToIr parsed_ir
+--   checkProgramWellTyped ir
+--   return ir
 main = do
   parsed_ir <- Parser.parseFile "examples/test.ulm"
-  case checkAndType parsed_ir of
-    Left err -> putStrLn $ "Error: " ++ show err
+  ir <-
+    case parsedProgramToIr parsed_ir of
+      Left err -> do
+        putStrLn $ "Error: " ++ show err
+        exitFailure
+      Right res -> return res
+  case typeProgram ir of
+    Left err -> do
+      putStrLn $ "Error: " ++ show err
+      exitFailure
     Right res -> putStrLn $ show res
