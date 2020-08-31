3c408ebbc51e05af798cad49595d3e3dbaa9f947
DwarfMaster <luc@dwarfmaster.net>
2020-08-31 23:41:37 +0200

    Made repl the main

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,3 @@
-import Parser
-import ParsingToIR
-import System.Exit
-import Typing
+import Repl
 
--- checkAndType parsed_ir = do
---   ir <- parsedProgramToIr parsed_ir
---   checkProgramWellTyped ir
---   return ir
-main = do
-  parsed_ir <- Parser.parseFile "examples/test.ulm"
-  ir <-
-    case parsedProgramToIr parsed_ir of
-      Left err -> do
-        putStrLn $ "Error: " ++ show err
-        exitFailure
-      Right res -> return res
-  case typeProgram ir of
-    Left err -> do
-      putStrLn $ "Error: " ++ show err
-      exitFailure
-    Right res -> putStrLn $ show res
+main = repl
